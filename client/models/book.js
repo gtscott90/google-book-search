89b69b7a051da81ae0bcb1ed232a92a560946d08
Greg Scott <gtscott90@gmail.com>
2021-04-14 00:17:55 -0400

    columns for mongoose schema added

--- client/models/book.js
+++ client/models/book.js
@@ -2,7 +2,21 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-
+    title: {
+        type: String,
+    },
+    author: {
+        type: String,
+    },
+    description: {
+        type: String,
+    },
+    image: {
+        type: String,
+    },
+    link: {
+        type: String,
+    },
 })
 
 module.exports = mongoose.model("Book", bookSchema);
