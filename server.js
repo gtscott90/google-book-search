eea223f939778922ffa308ec18af9e5a19203643
Greg Scott <gtscott90@gmail.com>
2021-04-14 00:24:41 -0400

    connected mongoose in the server file and made the routes folder

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mongoose = require("mongoose");
+const routes = require("./routes")
 
 // Define middleware here
 app.use(express.urlencoded({ extended: true }));
@@ -12,8 +14,16 @@ if (process.env.NODE_ENV === "production") {
 }
 
 // Define API routes here
+app.use(routes)
 
-// Send every other request to the React app
+// Mongoose Connection
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
+
 // Define any API routes before this runs
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
